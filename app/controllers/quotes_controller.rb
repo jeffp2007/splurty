775dfe9d18f18c07231322fe3d316a94f0e8c7dd
Jeff Powers <jeffp2007@gmail.com>
2015-06-13 18:35:41 +0000

    Fixed error message displays for failing/successfully adding a new quote

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,29 +1,39 @@
 class QuotesController < ApplicationController
+
+	before_filter :characters
+
 	def index
 		@quote = Quote.order("RANDOM()").first
-		@characters = ['Andy Bernard', 'Angela Martin', 'Clark Green', 'Creed Bratton', 'Darryl Philbin', 'David Wallace', 'Dwight Schrute', 'Erin Hannon',' Gabe Lewis', 'Holly Flax', 'Jan Levinson', 'Jim Halpert', 'Karen Filippelli', 'Kelly Kapoor', 'Kevin Malone', 'Meredith Palmer', 'Michael Scott', 'Nellie Bertram', 'Oscar Martinez', 'Pam Beesly', 'Pete Miller', 'Phyllis Lapin', 'Robert California', 'Roy Anderson', 'Ryan Howard', 'Stanley Hudson', 'Toby Flenderson']
 	end
 
 	def create
 	    @quote = Quote.create(quote_params)
 	    if @quote.invalid?
-	      flash[:error] = '<strong>Could not save</strong> the data you entered is invalid.'
-	    end
-		redirect_to thanks_path
+	    	flash[:error] = '<strong>Could not save.</strong> The data you entered is invalid.'
+	    	redirect_to root_path
+		else
+			flash[:success] = 'You successfully added the following quote from The Office.'
+			redirect_to thanks_path
+		end
 	end
 
 	def about
 	end
 
 	def thanks
-		@added_quote = Quote.last
+		@last_quote = Quote.last
 	end
+
 
 
 	private
 
 	def quote_params
 		params.require(:quote).permit(:saying, :author)
+	end
+
+	def characters
+		@characters = ['Andy Bernard', 'Angela Martin', 'Clark Green', 'Creed Bratton', 'Darryl Philbin', 'David Wallace', 'Dwight Schrute', 'Erin Hannon',' Gabe Lewis', 'Holly Flax', 'Jan Levinson', 'Jim Halpert', 'Karen Filippelli', 'Kelly Kapoor', 'Kevin Malone', 'Meredith Palmer', 'Michael Scott', 'Nellie Bertram', 'Oscar Martinez', 'Pam Beesly', 'Pete Miller', 'Phyllis Lapin', 'Robert California', 'Roy Anderson', 'Ryan Howard', 'Stanley Hudson', 'Toby Flenderson']
 	end
 
 end
