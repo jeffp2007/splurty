5452f30a1b05bfad376468813391e383fe4deee6
Jeff Powers <jeffp2007@gmail.com>
2015-08-09 20:36:56 +0000

    Building a model test

--- test/models/quote_test.rb
+++ test/models/quote_test.rb
@@ -2,8 +2,11 @@ require 'test_helper'
 
 class QuoteTest < ActiveSupport::TestCase
 
-	test "the truth" do
-		assert true
+	test "unique_tag" do
+		quote = Quote.create(:author => 'Michael Scott', :saying => 'I want some milk.')
+		expected = 'MS#' + quote.id.to_s
+		actual = quote.unique_tag
+		assert_equal expected, actual
 	end
   # test "the truth" do
   #   assert true
